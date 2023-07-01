a2f55530bc97d665f35000aef1e375ec893e3124
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2023-07-01 04:01:00 -0300

    Cambiada logica

--- jugadores.c
+++ jugadores.c
@@ -1,49 +1,46 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Jugador{
-    char NyA[51]; //Nombre y Apellido
-    int nro; // Numero de camiseta
-    int goles; // Goles totales
+#define MAX_JUGADORES 35
+
+struct jugador {
+    char NyA[50]; //Nombre y Apellido
+    int nro; //Numero de Camiseta
+    int goles; //Goles totales
     int pasesOk; //Pases acertados
-    int pasesTot; // pases totales
-    int minutosTotales; // Cantidad de minutos Jugados
-    char posicion; // 'a' aquero - 'j' jugador
+    int pasesTot; // Pases totales
+    int minutosTotales; //cantidad de minutos jugados
+    char posicion; //'a' arquero - 'j' jugador
 };
 
+int main() {
+    struct jugador jugadores[MAX_JUGADORES];
+    int cant_jugadores = 0;
 
-int main()
-{
-    struct Jugador jugadores[35];
-    int i = 0;
-    int continuar = 0;
-    while(i < 35 && continuar == 0){
-        char nombreAux[51];
-        printf("Ingresa tu nombre y apellido: ");
-        fgets(nombreAux, sizeof(nombreAux), stdin);
-        
-        // Elimina el carácter de nueva línea del final de la cadena ingresada
-        nombreAux[strcspn(nombreAux, "\n")] = '\0';
-        
-        if(strcmp(nombreAux, "fin") != 0){
-            strcpy(jugadores[i].NyA, nombreAux);
-            
-            printf("Ingresa el numero de camiseta: ");
-            scanf("%d", &jugadores[i].nro);
+    while (cant_jugadores < MAX_JUGADORES) {
+        printf("Ingrese un NyA (o 'fin' para salir): ");
+        fgets(jugadores[cant_jugadores].NyA, sizeof(jugadores[cant_jugadores].NyA), stdin);
+        jugadores[cant_jugadores].NyA[strcspn(jugadores[cant_jugadores].NyA, "\n")] = '\0';  // Eliminar el carácter de nueva línea
+
+        if (strcmp(jugadores[cant_jugadores].NyA, "fin") == 0) {
+            break;  // Salir del bucle si se ingresa "fin"
         }
-        else{
-            continuar = 1; // Se termina el bucle
-        }
-        
-        i++;
+
+        printf("Ingrese el numero de camiseta: ");
+        scanf("%d", &jugadores[cant_jugadores].nro);
+        getchar();  // Capturar el carácter de nueva línea después de scanf
+
+        cant_jugadores++;
     }
-    
-    //Mostrar los jugadores
-    for(int i = 0; i < 35; i++){
-        printf("Datos del jugador:\n", i);
-        printf("Nombre y Apellido: %s\n", jugadores[i].NyA);
-        printf("Numero de camiseta: %d\n", jugadores[i].nro);
+
+    printf("\nJugadores ingresados:\n");
+
+    for (int i = 0; i < cant_jugadores; i++) {
+        printf("Nombre y apellido: %s\n", jugadores[i].NyA);
+        printf("Número de camiseta: %d\n\n", jugadores[i].nro);
     }
-    
+
+    printf("¡Fin del programa!\n");
+
     return 0;
 }
