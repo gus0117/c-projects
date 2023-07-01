9e5487b695bc3a20f1683a1337a0e7660820a6f4
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2023-07-01 17:10:16 -0300

    added more properties to print

--- jugadores.c
+++ jugadores.c
@@ -15,10 +15,13 @@ struct jugador {
 
 int main() {
     struct jugador jugadores[MAX_JUGADORES];
+    struct jugador ordenGoles[MAX_JUGADORES];
+    struct jugador ordenEfectividad[MAX_JUGADORES];
+    
     int cant_jugadores = 0;
 
     while (cant_jugadores < MAX_JUGADORES) {
-        printf("Ingrese un NyA (o 'fin' para salir): ");
+        printf("Ingrese nombre y apellido (o 'fin' para salir): ");
         fgets(jugadores[cant_jugadores].NyA, sizeof(jugadores[cant_jugadores].NyA), stdin);
         jugadores[cant_jugadores].NyA[strcspn(jugadores[cant_jugadores].NyA, "\n")] = '\0';  // Eliminar el carácter de nueva línea
 
@@ -28,7 +31,27 @@ int main() {
 
         printf("Ingrese el numero de camiseta: ");
         scanf("%d", &jugadores[cant_jugadores].nro);
-        getchar();  // Capturar el carácter de nueva línea después de scanf
+        getchar();
+        
+        printf("Ingrese goles totales: ");
+        scanf("%d", &jugadores[cant_jugadores].goles);
+        getchar();
+        
+        printf("Ingrese cantidad de pases acertados: ");
+        scanf("%d", &jugadores[cant_jugadores].pasesOk);
+        getchar();
+        
+        printf("Ingrese cantidad de pases totales: ");
+        scanf("%d", &jugadores[cant_jugadores].pasesTot);
+        getchar();
+        
+        printf("Ingrese minutos totales: ");
+        scanf("%d", &jugadores[cant_jugadores].minutosTotales);
+        getchar();
+        
+        printf("Ingrese la posicion del jugador: ");
+        scanf("%c", &jugadores[cant_jugadores].posicion);
+        getchar();
 
         cant_jugadores++;
     }
@@ -37,8 +60,26 @@ int main() {
 
     for (int i = 0; i < cant_jugadores; i++) {
         printf("Nombre y apellido: %s\n", jugadores[i].NyA);
-        printf("Número de camiseta: %d\n\n", jugadores[i].nro);
+        printf("Número de camiseta: %d\n", jugadores[i].nro);
+        printf("Goles totales: %d\n", jugadores[i].goles);
+        printf("Pases acertados: %d\n", jugadores[i].pasesOk);
+        printf("Pases totales: %d\n", jugadores[i].pasesTot);
+        printf("Minutos totales: %d\n", jugadores[i].minutosTotales);
+        if(jugadores[i].posicion == 'a'){
+            printf("Posicion: %s\n\n", "arquero");
+        }
+        if(jugadores[i].posicion == 'j'){
+            printf("Posicion: %s\n\n", "jugador");
+        }
+        
     }
+    
+    // a. Ordenar por la cantidad de goles
+    for(int i = 0; i < cant_jugadores; i++){
+        for(int j = 0; j < cant_jugaadores; i++){
+            if()
+        }
+    }    
 
     printf("¡Fin del programa!\n");
 
